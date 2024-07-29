5ae5cecaee059026393c958ad81acf985c9d0088
Ron Ochakovsky <ron.ochak@gmail.com>
2024-07-29 13:51:28 +0300

    implemented 'word' class

--- Word.java
+++ Word.java
@@ -2,102 +2,59 @@ package test;
 
 
 public class Word {
-//    private Tile[] tiles;
-//    private int score;
-//
-//    public Word(Tile[] tiles) {
-//        this.tiles = tiles;
-//        this.score = 0;
-//        for (Tile t : tiles) {
-//            this.score += t.getScore();
-//        }
-//    }
-//
-//    public int getScore() {
-//        return this.score;
-//    }
-//
-//    public Tile[] getTiles() {
-//        return this.tiles;
-//    }
-//
-//    public String toString() {
-//        String s = "";
-//        for (Tile t : this.tiles) {
-//            s += t.getLetter();
-//        }
-//        return s;
-//    }
-//
-//    public boolean equals(Word w) {
-//        if (this.tiles.length != w.getTiles().length) {
-//            return false;
-//        }
-//        for (int i = 0; i < this.tiles.length; i++) {
-//            if (!this.tiles[i].equals(w.getTiles()[i])) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    public boolean contains(Tile t) {
-//        for (Tile tile : this.tiles) {
-//            if (tile.equals(t)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    public boolean contains(Word w) {
-//        for (Tile tile : w.getTiles()) {
-//            if (!this.contains(tile)) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    public boolean isSubWord(Word w) {
-//        if (this.tiles.length < w.getTiles().length) {
-//            return false;
-//        }
-//        for (int i = 0; i < this.tiles.length - w.getTiles().length + 1; i++) {
-//            boolean isSubWord = true;
-//            for (int j = 0; j < w.getTiles().length; j++) {
-//                if (!this.tiles[i + j].equals(w.getTiles()[j])) {
-//                    isSubWord = false;
-//                    break;
-//                }
-//            }
-//            if (isSubWord) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    public boolean isSuperWord(Word w) {
-//        return w.isSubWord(this);
-//    }
-//
-//    public boolean isAnagram(Word w) {
-//        if (this.tiles.length != w.getTiles().length) {
-//            return false;
-//        }
-//        int[] quantities = new int[26];
-//        for (Tile tile : this.tiles) {
-//            quantities[tile.getLetter() - 'A']++;
-//        }
-//        for (Tile tile : w.getTiles()) {
-//            quantities[tile.getLetter() - 'A']--;
-//        }
-//        for (int i = 0; i < 26; i++) {
-//            if (quantities[i] != 0) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
+    private Tile[] tiles;
+    private int row, col;
+    private boolean vertical;
+
+    public Word(Tile[] tiles, int row, int col, boolean vertical) {
+        this.tiles = tiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public boolean isVertical() {
+        return this.vertical;
+    }
+
+    public Tile[] getTiles() {
+        return this.tiles;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (Tile t : this.tiles) {
+            s += t.getLetter();
+        }
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object w) {
+        if (this == w) {
+            return true;
+        }
+        if (w == null || w.getClass() != getClass()) {
+            return false;
+        }
+        Word other = (Word) w;
+        if (this.row != other.row || this.col != other.col || this.vertical != other.vertical) {
+            return false;
+        }
+        for (int i = 0; i < this.tiles.length; i++) {
+            if (!this.tiles[i].equals(other.tiles[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
